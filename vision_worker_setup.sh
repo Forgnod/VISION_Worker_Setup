1125ce82da97d5648771b0402cebd0d8200a65dc
Forgnod Schergenglueck <39371598+Forgnod@users.noreply.github.com>
2019-10-27 02:10:10 +0200

    Create vision_worker_setup.sh

---
+++ vision_worker_setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# System updaten
+
+  sudo apt update
+
+# Grundlegende Programm
+
+  sudo apt-get install htop ufw
+
+# ufw einstellungen
+		
+    sudo apt-get update
+    sudo apt-get install ufw
+    sudo ufw limit 22/tcp         #aktueller SSH Port
+    sudo ufw allow 8834/tcp       #neuer SSH Port
+    sudo ufw limit 8834/tcp     
+    sudo ufw allow 3777/tcp       #Port IP-Symcon
+		sudo ufw enable
+    
+# Installation IP-Symcon (Weingaertner VISION)
+
+# Repository hinzufügen
+    
+    cd /etc/apt
+    sudo cat >> sources.list <<'EOF'
+    > deb http://apt.symcon.de/ stable rpi
+    
+# GPG Schlüssel
+    
+    wget -qO - http://apt.symcon.de/symcon.key | sudo apt-key add -
+    
+# Symcon installieren
+
+    sudo apt-get update
+    sudo apt-get install update
